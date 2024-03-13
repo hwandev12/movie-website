a8d81a41fc2af8b4d60977e0a269b70c8ed09995
hwandev12 <husan.ahmedov777@yandex.com>
2024-03-13 21:55:17 +0500

    add video details

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,22 @@
+let html = document.querySelector("html");
+var navbar = document.querySelector(".home__navigation-wrapper");
+
 window.addEventListener("scroll", function () {
-  var navbar = document.querySelector(".home__navigation-wrapper");
   if (window.scrollY > 100) {
-    navbar.classList.add("navbar-scrolled");
+    if (html.classList.contains("dark")) {
+      navbar.classList.add("navbar-scrolled");
+    } else {
+      navbar.classList.add("navbar-scrolled-light-scroll");
+    }
   } else {
-    navbar.classList.remove("navbar-scrolled");
+    navbar.classList.remove("navbar-scrolled") ^
+      navbar.classList.remove("navbar-scrolled-light");
+    navbar.classList.remove("navbar-scrolled-light-scroll");
   }
 });
 
 var descriptionElement = document.querySelector(".last_p");
-if(descriptionElement) {
+if (descriptionElement) {
   var description = descriptionElement.innerHTML;
   var truncatedDescription = truncate(description, 14);
   descriptionElement.innerHTML = truncatedDescription;
@@ -23,13 +31,16 @@ function truncate(text, limit) {
   }
 }
 
-let html = document.querySelector("html");
 let lightButton = document.querySelector(".home__navigation .fa-sun");
 let nightButton = document.querySelector(".home__navigation .fa-moon");
 
 lightButton.addEventListener("click", () => {
   html.classList.remove("dark");
+  navbar.classList.add("navbar-scrolled-light");
 });
 nightButton.addEventListener("click", () => {
   html.classList.add("dark");
+  navbar.classList.add("navbar-scrolled");
+  navbar.classList.remove("navbar-scrolled-light");
+  navbar.classList.remove("navbar-scrolled-light-scroll");
 });
