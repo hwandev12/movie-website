d9b78daf42e7bc7fd6db399403ac70a98e680f10
hwandev12 <husan.ahmedov777@yandex.com>
2024-03-17 12:30:52 +0500

    fix views

--- apps/series/views.py
+++ apps/series/views.py
@@ -11,15 +11,29 @@ class SerieDetailView(generic.DetailView):
     pk_url_kwarg = 'serieID'
 
     def get_context_data(self, **kwargs):
+        """
+        Agar qo'yilayogan serial barcha qismlar deb qo'ysa unda 
+        fasllarni tanlay oladigon bo'lishi kerak
+        """
         context = super().get_context_data(**kwargs)
         serie = self.model.objects.get(id=self.kwargs.get("serieID", ""))
-        
+        season_number = []
+        episodes = serie_models.Episode.objects.all().filter(series=serie)
+
+        for e in episodes:
+            v_list = e.season_number
+            season_number.append(v_list)
+            
         request_episode = self.request.GET.get("season-number-got")
         if not request_episode:
-            episodes = serie_models.Episode.objects.all().filter(series=serie)
+            episodes = serie_models.Episode.objects.all().filter(series=serie, season_number=1)
         else:
             request_episode = int(request_episode)
-            episodes = serie_models.Episode.objects.all().filter(series=serie, season_number=request_episode)
+            episodes = serie_models.Episode.objects.all().filter(
+                series=serie, season_number=request_episode)
+        print(self.request.get_full_path())
+        context['season_number'] = tuple(set(season_number))
+        context['request_episode'] = request_episode
         context['categories'] = entry_models.Category.objects.all()
         context['episodes'] = episodes
         return context
