258a5323696a6a462799aa799d2a2886fddf4651
hwandev12 <husan.ahmedov777@yandex.com>
2024-03-18 23:47:52 +0500

    add movies list page

--- apps/movie/urls.py
+++ apps/movie/urls.py
@@ -5,5 +5,6 @@ app_name='movie'
 
 urlpatterns = [
     path("single/<slug:slug>/", views.movie_single, name='single'),
+    path("barcha-tarjima-filmlar/", views.movies_list_view, name='movies_list'),
     path("ajax-single-movie-get/<int:ID>/", views.get_single_video, name='single_video_ajax')
 ]
--- apps/movie/views.py
+++ apps/movie/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
 from django.http import JsonResponse
@@ -10,17 +11,19 @@ from apps.series import models as serie_models
 
 import json
 
+
 class MovieSingle(generic.DetailView):
     model = models.Movie
     template_name = 'movies/single_movie.html'
     context_object_name = 'movie'
     pk_url_kwarg = 'movieID'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = entry_models.Category.objects.all()
         return context
-    
+
+
 def get_single_video(request, ID):
     is_movie = request.GET.get("is_movie")
     if is_movie == 'movie':
@@ -37,6 +40,25 @@ def get_single_video(request, ID):
         serie_trailer = serializers.serialize("json", [serie_trailer])
         data = {"id": ID, "serie_ajax": serie, "serie_trailer": serie_trailer}
         return JsonResponse(data, safe=False)
+
+
+class MoviesListPage(generic.ListView):
+    paginate_by = 10
+    model = movie_models.Movie
+    context_object_name = 'movies'
+    template_name = 'movies/movies_list.html'
     
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.order_by("-time_created")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = entry_models.Category.objects.all()
+        return context
+    
+
+
 # make classes to function names
 movie_single = MovieSingle.as_view()
+movies_list_view = MoviesListPage.as_view()
