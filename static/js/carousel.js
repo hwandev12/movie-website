acd93495d26337f945bca3cb94b8aba81d809483
hwandev12 <husan.ahmedov777@yandex.com>
2024-03-18 11:32:12 +0500

    update carousel for gestures

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,106 +1,170 @@
-let carouselContainer = document.querySelector(".home__carousel");
-let carouselItem = document.querySelectorAll(".carousel__item");
-let leftSliderButton = document.querySelector(
-  ".home__carousel .fa-chevron-left"
-);
-let rightSliderButton = document.querySelector(
-  ".home__carousel .fa-chevron-right"
-);
+document.addEventListener("DOMContentLoaded", function () {
+  let carouselContainer = document.querySelector(".home__carousel");
+  let carouselItem = document.querySelectorAll(".carousel__item");
+  let leftSliderButton = document.querySelector(
+    ".home__carousel .fa-chevron-left"
+  );
+  let rightSliderButton = document.querySelector(
+    ".home__carousel .fa-chevron-right"
+  );
 
-let counterCarousel = 0;
+  let counterCarousel = 0;
 
-var circleContainer = document.querySelectorAll(".circle");
-var circleContainerWrapper = document.querySelector(".circle_container");
-if (circleContainer) {
-  function createCircles() {
-    circleContainerWrapper.innerHTML = ""; // Clear existing circles
-    for (var i = 0; i < carouselItem.length; i++) {
-      var circle = document.createElement("span");
-      circle.classList.add("circle");
-      circle.dataset.index = i;
-      circleContainerWrapper.appendChild(circle);
+  var circleContainer = document.querySelectorAll(".circle");
+  var circleContainerWrapper = document.querySelector(".circle_container");
+  if (circleContainer) {
+    function createCircles() {
+      circleContainerWrapper.innerHTML = ""; // Clear existing circles
+      for (var i = 0; i < carouselItem.length; i++) {
+        var circle = document.createElement("span");
+        circle.classList.add("circle");
+        circle.dataset.index = i;
+        circleContainerWrapper.appendChild(circle);
+      }
     }
   }
-}
 
-// Initial creation of circles
-createCircles();
+  // Initial creation of circles
+  createCircles();
 
-function updateCircles() {
-  var circleContainer = document.querySelectorAll(".circle");
-  circleContainer.forEach((circle, index) => {
-    if (index == counterCarousel) {
-      circle.classList.add("circle_full");
-    } else {
-      circle.classList.remove("circle_full");
-    }
+  function updateCircles() {
+    var circleContainer = document.querySelectorAll(".circle");
+    circleContainer.forEach((circle, index) => {
+      if (index == counterCarousel) {
+        circle.classList.add("circle_full");
+      } else {
+        circle.classList.remove("circle_full");
+      }
+    });
+  }
+
+  // Initial update of circles
+  updateCircles();
+
+  if (circleContainerWrapper) {
+    circleContainerWrapper.addEventListener("click", (event) => {
+      if (event.target.classList.contains("circle")) {
+        counterCarousel = parseInt(event.target.dataset.index);
+        changeImage();
+      }
+    });
+  }
+
+  leftSliderButton.addEventListener("click", () => {
+    counterCarousel -= 1;
+    clearInterval(animationCarousel);
+    animationCarousel = setInterval(nextImage, 5000);
+    changeImage();
   });
-}
 
-// Initial update of circles
-updateCircles();
+  rightSliderButton.addEventListener("click", () => {
+    counterCarousel += 1;
+    clearInterval(animationCarousel);
+    animationCarousel = setInterval(nextImage, 5000);
+    changeImage();
+  });
 
-if (circleContainerWrapper) {
-  circleContainerWrapper.addEventListener("click", (event) => {
-    if (event.target.classList.contains("circle")) {
-      counterCarousel = parseInt(event.target.dataset.index);
+  function nextImage() {
+    counterCarousel += 1;
+    changeImage();
+  }
+
+  const changeImage = () => {
+    if (counterCarousel > carouselItem.length - 1) {
+      counterCarousel = 0;
+    } else if (counterCarousel < 0) {
+      counterCarousel = carouselItem.length - 1;
+    }
+    carouselItem.forEach((a, b) => {
+      a.style.transform = `translateX(-${counterCarousel * 100}%)`;
+      let p = a.querySelectorAll("p");
+      let h1 = a.querySelector("h1");
+      let dv = a.querySelector(".a_wrapper");
+      let dvr = a.querySelector(".carousel__item-right");
+      let elements = [h1, dv, dvr];
+      if (b == counterCarousel) {
+        h1.classList.add("active");
+        dv.classList.add("active");
+        dvr.classList.add("activeC");
+        p.forEach((l) => {
+          l.classList.add("active");
+        });
+      } else {
+        h1.classList.remove("active");
+        dv.classList.remove("active");
+        dvr.classList.remove("activeC");
+        p.forEach((l) => {
+          l.classList.remove("active");
+        });
+      }
+    });
+    updateCircles();
+  };
+
+  // gesture carousel
+  let touchStartX = 0;
+  let touchEndX = 0;
+
+  carouselContainer.addEventListener("touchstart", function (e) {
+    touchStartX = e.touches[0].clientX;
+  });
+
+  carouselContainer.addEventListener("touchmove", function (e) {
+    touchEndX = e.touches[0].clientX;
+  });
+
+  carouselContainer.addEventListener("touchend", function () {
+    const touchDiff = touchStartX - touchEndX;
+    if (
+      touchDiff > 50 &&
+      counterCarousel < carouselContainer.children.length - 1
+    ) {
+      counterCarousel++;
+      clearInterval(animationCarousel);
+      animationCarousel = setInterval(nextImage, 5000);
+      changeImage();
+    } else if (touchDiff < -50 && counterCarousel > 0) {
+      counterCarousel--;
+      clearInterval(animationCarousel);
+      animationCarousel = setInterval(nextImage, 5000);
       changeImage();
     }
   });
-}
 
-leftSliderButton.addEventListener("click", () => {
-  counterCarousel -= 1;
-  clearInterval(animationCarousel);
-  animationCarousel = setInterval(nextImage, 5000);
-  changeImage();
-});
+  let isDragging = false;
+  let dragStartX = 0;
+  let dragEndX = 0;
 
-rightSliderButton.addEventListener("click", () => {
-  counterCarousel += 1;
-  clearInterval(animationCarousel);
-  animationCarousel = setInterval(nextImage, 5000);
-  changeImage();
-});
+  carouselContainer.addEventListener("mousedown", function (e) {
+    isDragging = true;
+    dragStartX = e.clientX;
+  });
 
-function nextImage() {
-  counterCarousel += 1;
-  changeImage();
-}
-
-const changeImage = () => {
-  if (counterCarousel > carouselItem.length - 1) {
-    counterCarousel = 0;
-  } else if (counterCarousel < 0) {
-    counterCarousel = carouselItem.length - 1;
-  }
-  carouselItem.forEach((a, b) => {
-    a.style.transform = `translateX(-${counterCarousel * 100}%)`;
-    let p = a.querySelectorAll("p");
-    let h1 = a.querySelector("h1");
-    let dv = a.querySelector(".a_wrapper");
-    let dvr = a.querySelector(".carousel__item-right");
-    let elements = [h1, dv, dvr];
-    if (b == counterCarousel) {
-      h1.classList.add("active");
-      dv.classList.add("active");
-      dvr.classList.add("activeC");
-      p.forEach((l) => {
-        l.classList.add("active");
-      });
-    } else {
-      h1.classList.remove("active");
-      dv.classList.remove("active");
-      dvr.classList.remove("activeC");
-      p.forEach((l) => {
-        l.classList.remove("active");
-      });
+  carouselContainer.addEventListener("mousemove", function (e) {
+    if (isDragging) {
+      dragEndX = e.clientX;
     }
   });
-  updateCircles();
-};
 
-let animationCarousel = setInterval(() => {
-  counterCarousel += 1;
-  changeImage();
-}, 5000);
+  carouselContainer.addEventListener("mouseup", function () {
+    isDragging = false;
+    const dragDiff = dragStartX - dragEndX;
+    if (
+      dragDiff > 50 &&
+      counterCarousel < carouselContainer.children.length - 1
+    ) {
+      counterCarousel++;
+      clearInterval(animationCarousel);
+      animationCarousel = setInterval(nextImage, 5000);
+      changeImage();
+    } else if (dragDiff < -50 && counterCarousel > 0) {
+      counterCarousel--;
+      changeImage();
+    }
+  });
+
+  let animationCarousel = setInterval(() => {
+    counterCarousel += 1;
+    changeImage();
+  }, 5000);
+});
